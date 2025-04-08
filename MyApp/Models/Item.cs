e11796ed832cfbe09ed1cc316f63956f5a6d333d
Doğukan Özçelik <dogukanozcelik673@gmail.com>
2025-04-09 02:06:32 +0300

    Add project files.

---
+++ MyApp/Models/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace MyApp.Models
+{
+    public class Item
+    {
+        public required List<int> Numbers { get; set; }
+        public int? SelectedNumber { get; set; }
+        public int CorrectNumber { get; set; }
+        public string Message { get; set; }
+    }
+}
